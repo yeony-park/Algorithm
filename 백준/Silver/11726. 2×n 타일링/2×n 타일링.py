203acf48234bbe452a9b048b3306fdb0a029263b
Yeonjeong Park <152824499+yeony-park@users.noreply.github.com>
2026-03-18 00:02:28 +0900

    [Silver III] Title: 2×n 타일링, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,18 +1,11 @@
-import sys
-
-input = sys.stdin.readline
-
 n = int(input())
-d = [0]*(n+1)
 
-d[1] = 1
-if n >=2 :
-    d[2] = 2
+dp = [0]*(n+1)
+dp[1]=1
+if n >= 2:
+  dp[2]=2
 
-def tile(num):
-    if d[num] != 0:
-        return d[num]
-    d[num] = tile(num-1) + tile(num-2)
-    return d[num]
+for i in range(3,n+1):
+  dp[i] = (dp[i-1]+dp[i-2])%10007
 
-print(tile(n)%10007)
+print(dp[n])
